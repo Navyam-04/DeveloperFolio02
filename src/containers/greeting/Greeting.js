dadf8a508013cb6439cb9cc6489f53aa4ffd30ca
Navyam-04 <middenavya51@gmail.com>
2025-01-18 17:53:59 +0530

    Pdf updation I mean resume

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./NavyaResume.pdf")}
+                    href={require("./NavyaResume01.pdf")}
                     download="NavyaResume.pdf"
                     className="download-link-button"
                   >
